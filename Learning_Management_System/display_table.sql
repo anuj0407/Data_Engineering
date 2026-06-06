817ac8b3ccc8b186c328e93134dd0f7705566a6b
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-06-06 12:13:25 +0530

    [Anuj chaturvedi] add. querry for displaying all tables

---
+++ Learning_Management_System/display_table.sql
@@ -0,0 +1,9 @@
+SELECT * FROM users;
+SELECT * FROM courses;
+SELECT * FROM lessons;
+SELECT * FROM enrollments;
+SELECT * FROM user_activity;
+SELECT * FROM assessments;
+SELECT * FROM assessment_submissions;
+
+
